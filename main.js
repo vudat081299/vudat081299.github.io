90df791afec077fb78cef89aae8c23c4a312b1a5
vudat81299 <vudat81299@gmail.com>
2021-10-01 20:02:45 +0700

    login form, signup form

--- main.js
+++ main.js
@@ -4,49 +4,49 @@ function mount() {
   let data = [
     {
       tag: "Concurrency",
-      type: "",
+      type: "toggles2",
       description:
         "Swift has built-in support for writing asynchronous and parallel code in a structured way. Asynchronous code can be suspended and resumed later, although only one piece of the program executes at a time.",
     },
     {
       tag: "Defining and Calling Asynchronous Functions",
-      type: "",
+      type: "code-slash",
       description:
         "An asynchronous function or asynchronous method is a special kind of function or method that can be suspended while it’s partway through execution.",
     },
     {
       tag: "Asynchronous Sequences",
-      type: "",
+      type: "controller",
       description:
         "The listPhotos(inGallery:) function in the previous section asynchronously returns the whole array at once, after all of the array’s elements are ready.",
     },
     {
       tag: "Calling Asynchronous Functions in Parallel",
-      type: "",
+      type: "droplet-fill",
       description:
         "Calling an asynchronous function with await runs only one piece of code at a time. While the asynchronous code is running, the caller waits for that code to finish before moving on to run the next line of code.",
     },
     {
       tag: "Tasks and Task Groups",
-      type: "",
+      type: "envelope-open-fill",
       description:
         "A task is a unit of work that can be run asynchronously as part of your program. All asynchronous code runs as part of some task. The async-let syntax described in the previous section creates a child task for you.",
     },
     {
       tag: "Unstructured Concurrency",
-      type: "",
+      type: "github",
       description:
         "In addition to the structured approaches to concurrency described in the previous sections, Swift also supports unstructured concurrency.",
     },
     {
       tag: "Task Cancellation",
-      type: "",
+      type: "shield-fill-check",
       description:
         "Swift concurrency uses a cooperative cancellation model. Each task checks whether it has been canceled at the appropriate points in its execution, and responds to cancellation in whatever way is appropriate.",
     },
     {
       tag: "Actors",
-      type: "",
+      type: "umbrella",
       description:
         "Like classes, actors are reference types, so the comparison of value types and reference types in Classes Are Reference Types applies to actors as well as classes. ",
     },
@@ -99,11 +99,17 @@ function reloadWithData(value) {
     element.classList.add("d-flex");
     element.classList.add("align-items-start");
     element.innerHTML =
-      '<div class="icon-square bg-light text-dark flex-shrink-0 me-3"><svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"/></svg></div><div><a href="posts-of-tag-page.html" class="text-dark text-decoration-none"><h4 class="fw-bold mt-2">' +
+      '<div class="icon-square bg-light text-dark flex-shrink-0 me-3"><i class="bi-' +
+      item.type +
+      '" width="1em" height="1em"></i></div><div><a href="posts-of-tag-page.html" class="text-dark text-decoration-none"><h4 class="fw-bold mt-2">' +
       item.tag +
       "</h4><p>" +
       item.description +
       "</p></a></div>";
+    // console.log(
+    //   '<div class="icon-square bg-light text-dark flex-shrink-0 me-3"><i class="bi-'item.type + ' me-2" width="1em" height="1em"></i></div><div><a href="posts-of-tag-page.html" class="text-dark text-decoration-none"><h4 class="fw-bold mt-2">' +
+    //   item.tag +"</h4><p>" + item.description +
+    //   "</p></a></div>");
     list.appendChild(element);
   });
 }
